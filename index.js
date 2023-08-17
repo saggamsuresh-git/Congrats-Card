5a547f7a145b334d54a532eb4c0a3944dc51cd1d
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 <saggamsuresh3@gmail.com>
2023-08-17 06:11:56 +0000

    In this project, let's build a Congrats Card usin React JS

---
+++ index.js
@@ -0,0 +1,17 @@
+const element = (
+  // Write your code here.
+  <div className="bg-container">
+    <h1>Congrstulations</h1>
+    <div className="card-container">
+      <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png" />
+      <h1>Kiran</h1>
+      <p>
+        Vishnu Institute of Computer Education and Technology.
+        <span>Bheemavaram</span>
+      </p>
+      <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png" />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
